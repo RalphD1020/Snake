1d27924a3f8a5564a7eacbc205282b6320e81d15
RalphD1020 <RalphDimayugasEmail@gmail.com>
2023-09-27 18:19:25 +0800

    Define Snake and Tile class. Initialize snake and grid

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,25 +2,47 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject tileCube;     // The prefab of the cube you want to instantiate
-    public int width = 20;          // Number of cubes in the X axis
-    public int height = 15;         // Number of cubes in the Z axis
+    public GameObject cubePrefab;
+    public GameObject snakePrefab;
+    public Snake snake;
+    public int width = 24;          // Number of cubes in the X axis
+    public int height = 16;         // Number of cubes in the Z axis
     public float spacing = 1.1f;    // Space between each cube
 
+    private Tile[,] _board;
+    
+    // todo: to update the positions cardinally, do x+/-1 * spacing or z+/-1 * spacing
+    // todo: keep reference to last cube in chain
+    // todo: keep reference to previous position
     void Start()
     {
-        GenerateGrid();
+        InstantiateBoard();
+        InitializeAndSpawnSnakeAtCenter();
     }
 
-    void GenerateGrid()
+    void InstantiateBoard()
     {
+        _board = new Tile[width, height];
         for (int x = 0; x < width; x++)
         {
             for (int z = 0; z < height; z++)
             {
-                Vector3 position = new Vector3(x * spacing, 0, z * spacing);
-                Instantiate(tileCube, position, Quaternion.identity, this.transform);
+                var position = new Vector3(x * spacing, 0, z * spacing);
+                var tileObject = Instantiate(cubePrefab, position, Quaternion.identity, this.transform);
+                var tileComponent = tileObject.AddComponent<Tile>();
+                tileComponent.Initialize(position);
+                _board[x, z] = tileComponent;
             }
         }
+    }
+    
+    private void InitializeAndSpawnSnakeAtCenter()
+    {
+        Vector3 tilePosition = _board[(width - 1)/2, (height - 1)/2].Position;
+        Vector3 snakePosition = new Vector3(tilePosition.x, 1, tilePosition.z);
+        var snakeObject = Instantiate(snakePrefab, snakePosition, Quaternion.identity, transform);
+        var snakeComponent = snakeObject.AddComponent<Snake>();
+        snakeComponent.Initialize(snakePosition);
+        snake = snakeComponent;
     }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Vector3 Position { get; set; }
+
+    public void Initialize(Vector3 position)
+    {
+        Position = position;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

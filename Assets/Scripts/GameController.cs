fd201fa4c5439854f697daf79eb62405e76a491a
RalphD1020 <RalphDimayugasEmail@gmail.com>
2023-09-28 07:42:01 +0800

    Code cleanup

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,18 +2,16 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject cubePrefab;
-    public GameObject snakePrefab;
-    public GameObject snakeInstance;
-    public Snake snake;
-    public int width = 24;          // Number of cubes in the X axis
-    public int height = 16;         // Number of cubes in the Z axis
-    public float spacing = 1.1f;    // Space between each cube
-
+    private GameObject _snake;
     private Tile currentTile;
     private Tile[,] _board;
     
-    // todo: to update the positions cardinally, do x+/-1 * spacing or z+/-1 * spacing
+    public GameObject cubePrefab;
+    public GameObject snakePrefab;
+    public int width = 24;          // Number of cubes in the X axis
+    public int height = 16;         // Number of cubes in the Z axis
+    public float spacing = 1.1f;    // Space between each cube
+    
     // todo: keep reference to last cube in chain
     // todo: keep reference to previous position
     
@@ -39,11 +37,28 @@ public class GameController : MonoBehaviour
 
     private void MoveSnake(int targetX, int targetZ)
     {
+        if (targetX >= _board.GetLength(0))
+        {
+            targetX = 0;
+        }
+        if (targetX < 0)
+        {
+            targetX += _board.GetLength(0);
+        }
+        
+        if (targetZ >= _board.GetLength(1))
+        {
+            targetZ = 0;
+        }
+        if (targetZ < 0)
+        {
+            targetZ += _board.GetLength(1);
+        }
+        
         var targetTile = _board[targetX, targetZ];
         var tilePosition = targetTile.Position;
         var snakePosition = new Vector3(tilePosition.x, 1, tilePosition.z);
-        snakeInstance.transform.position = snakePosition;
-        snake.Position = snakePosition;
+        _snake.transform.position = snakePosition;
         currentTile = targetTile;
     }
 
@@ -75,7 +90,6 @@ public class GameController : MonoBehaviour
         currentTile = _board[(width - 1) / 2, (height - 1) / 2];
         var tilePosition = currentTile.Position;
         var snakePosition = new Vector3(tilePosition.x, 1, tilePosition.z);
-        snakeInstance = Instantiate(snakePrefab, snakePosition, Quaternion.identity, transform);
-        snake.Initialize(snakePosition);
+        _snake = Instantiate(snakePrefab, snakePosition, Quaternion.identity, transform);
     }
 }
